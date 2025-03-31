05acf919efcec9b3b513a44a10d363406c8699ba
emacosta95 <emanuelecosta95@hotmail.com>
2025-03-31 18:46:15 +0200

    implement a correction to reduce the number of zero terms in the dictionary

--- src/utils_quasiparticle_approximation.py
+++ src/utils_quasiparticle_approximation.py
@@ -109,7 +109,81 @@ class QuasiParticlesConverter():
                 rest_idx+=1
                 
                 
+class QuasiParticlesConverterOnlynnpp():
+    
+    def __init__(self,):
+        pass
+    
+    def initialize_shell(self,state_encoding:List):
+        
+        #### nn and pp
+        couples=[]
+        for a,state_a in enumerate(state_encoding):
+            for b,state_b in enumerate(state_encoding):
+                
+                if b>a:
+                    _,_,ja,ma,_,tza=state_a
+                    _,_,jb,mb,_,tzb=state_b
+                    if ja==jb and ma==-mb and tza==tzb:
+                        couples.append([a,b])
+                        
 
+        self.couples=couples
+
+
+
+    def new_base_computation(self,base:np.ndarray):
+        
+        indices=np.nonzero(base)[0]
+        new_base=np.zeros(len(self.couples))
+        value=np.sum(base)
+    
+                
+        list_of_token_indices=[]
+        
+        for i in range(new_base.shape[0]):
+            
+            if base[self.couples[i][0]]+base[self.couples[i][1]]!=2 :
+                continue
+            else:
+                new_base[i]+=1
+                base[self.couples[i][0]]=0
+                base[self.couples[i][1]]=0
+        
+        if np.sum(new_base)==value//2:
+            return new_base
+        
+
+    def get_the_basis_matrix_transformation(self,basis:np.ndarray):
+        
+        self.quasiparticle_basis=[]
+        self.rest_basis=[]
+        
+        for i,b in enumerate(basis):
+            qp_base=self.new_base_computation(base=b)
+            
+            if qp_base is not(None):
+
+                self.quasiparticle_basis.append(qp_base)
+            else:
+                self.rest_basis.append(b)
+        self.quasiparticle_basis=np.asarray(self.quasiparticle_basis)
+        self.rest_basis=np.asarray(self.rest_basis)
+        
+        self.particles2quasiparticles=lil_matrix((self.quasiparticle_basis.shape[0],basis.shape[0]))
+        self.particles2restofstates=lil_matrix((self.rest_basis.shape[0],basis.shape[0]))
+        qp_idx=0
+        rest_idx=0
+        for i,b in enumerate(basis):
+            qp_base=self.new_base_computation(base=b)
+            
+            if qp_base is not(None):
+                self.particles2quasiparticles[qp_idx,i]=1.
+                qp_idx+=1
+            else:
+                self.particles2restofstates[rest_idx,i]=1
+                rest_idx+=1
+ 
 
 class HardcoreBosonsBasis:
 
